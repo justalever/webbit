d940cec2f83b3e6c3bb14633c741025399d410a3
Andy Leverenz <andy@justalever.com>
2022-04-06 10:26:56 -0500

    Add Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.erb",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: ["@tailwindcss/forms", "@tailwindcss/typography"],
+}
