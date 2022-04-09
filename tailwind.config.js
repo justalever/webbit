9aef6adf9f554bb8f0468181f9e8b267245a35f6
Andy Leverenz <andy@justalever.com>
2022-04-09 12:32:40 -0500

    Submissions show view

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: ["@tailwindcss/forms", "@tailwindcss/typography"],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 }
